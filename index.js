aa2756e82fe0b5a1e2db6b869733cd4be640da51
jennifer hodge <jenniferhodge@jennifers-Air.lan>
2023-11-23 19:16:59 -0600

    now on question 3, part 1

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 let rs = require("readline-sync");
+
+const keys = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
+
+// const shipKey = () => Math.floor(Math.random() * keys.length);
+
+// console.log(shipKey());
+
+// const gridLocation = keys[shipKey()];
+// console.log(gridLocation);
+
+const ships = [""];
+
+const pressAnyKey = require("press-any-key");
+pressAnyKey("Press any key to start the game:  ", {
+    
+//   ctrlC: "reject",
+}).then(function () {
+  console.log("Starting game.......");
+  let shipType = rs.question("Load first ship on the board: ", keys);
+
+  const question = rs.question("Load second ship on board: ", keys);
+
+
+  {
+  }
+
+  console.log("You chose: " + shipType + " and " + question);
+}); 
+
+
+
+// const question = rs.question("Enter a location to strike:");
+
+
+const shipKey = () => Math.floor(Math.random() * keys.length);
+
+console.log(shipKey());
+
+const gridLocation = keys[shipKey()];
+console.log(gridLocation);
+
+
+
+     
+
+// const shipStrike = rs.question("Enter a location to strike 'A2' ", keys, {
+  
+
+// });
